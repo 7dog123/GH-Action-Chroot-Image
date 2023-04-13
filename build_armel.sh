dc2b165c7f845e0c113a6d520fb5fa01821ffa7c
Talal <96490382+TalalMash@users.noreply.github.com>
2023-04-14 01:15:23 +0300

    Create build_armel.sh

---
+++ build_armel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+buildarch=armel
+apt install libext2fs2 debootstrap qemu-user-static wget
+fallocate -l 1.8G bullseye.img
+mkfs.ext4 bullseye.img
+mkdir mnt
+mount bulleseye.img mnt/
+cd mnt
+debootstrap --arch=$buildarch bullseye . http://ftp.de.debian.org/debian
+mount --bind /dev dev
+mount --bind /proc proc
+mount --bind /sys sys
+ln -s /bin/bash bin/ash
+umount dev
+umount proc
+umount sys
+cd ..
+umount mnt
+gzip bullseye.img
+mv bullseye.img.gz bullseye_armel.img.gz
